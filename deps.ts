0ed881d1f370bb0c57c5651080ac8d5d1c589423
mcgear <michael.gearhardt@gmail.com>
2024-11-12 23:32:27 -0700

    Ref updates

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as colors from 'jsr:@std/fmt@^1.0.0/colors';
-export * as path from 'jsr:@std/path@^1.0.2';
+export * as colors from 'jsr:@std/fmt@1.0.3/colors';
+export * as path from 'jsr:@std/path@1.0.8';
